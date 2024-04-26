412c38a3c50093ab52528e8deee1ac03c8fc000b
My Nguyen <chiffonng136@gmail.com>
2024-04-27 00:04:32 +0200

    Task 0.3 high-order functions

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -4,6 +4,7 @@ https://minitorch.github.io/module0/module0/
 """
 
 import math
+from functools import reduce as _reduce
 from typing import Callable, Iterable
 
 # ## Task 0.1
@@ -104,8 +105,8 @@ def inv(x: float) -> float:
 
 def inv_back(x: float, d: float) -> float:
     r"If $f(x) = 1/x$ compute $d \times f'(x)$"
-    # f'(x) = -1/x^2 => d * -1/x^2 => -d / (x * x) 
-    return -d / (x ** 2)
+    # f'(x) = -1/x^2 => d * -1/x^2 => -d / (x * x)
+    return -d / (x**2)
 
 
 def relu_back(x: float, d: float) -> float:
@@ -132,14 +133,12 @@ def map(fn: Callable[[float], float]) -> Callable[[Iterable[float]], Iterable[fl
         A function that takes a list, applies `fn` to each element, and returns a
          new list
     """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    return lambda ls: [fn(x) for x in ls]
 
 
 def negList(ls: Iterable[float]) -> Iterable[float]:
     "Use `map` and `neg` to negate each element in `ls`"
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    return map(neg)(ls)
 
 
 def zipWith(
@@ -158,21 +157,20 @@ def zipWith(
          applying fn(x, y) on each pair of elements.
 
     """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    return lambda ls1, ls2: [fn(x, y) for x, y in zip(ls1, ls2)]
 
 
 def addLists(ls1: Iterable[float], ls2: Iterable[float]) -> Iterable[float]:
     "Add the elements of `ls1` and `ls2` using `zipWith` and `add`"
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    return zipWith(add)(ls1, ls2)
 
 
 def reduce(
     fn: Callable[[float, float], float], start: float
 ) -> Callable[[Iterable[float]], float]:
     r"""
-    Higher-order reduce.
+    Higher-order reduce: apply a binary operation repeatedly to elements of a collection
+    (such as a list or an array) to produce a single result.
 
     Args:
         fn: combine two values
@@ -183,17 +181,14 @@ def reduce(
          $x_1 \ldots x_n$ and computes the reduction :math:`fn(x_3, fn(x_2,
          fn(x_1, x_0)))`
     """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    return lambda ls: _reduce(fn, ls, start)
 
 
 def sum(ls: Iterable[float]) -> float:
     "Sum up a list using `reduce` and `add`."
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    return reduce(add, 0.0)(ls)
 
 
 def prod(ls: Iterable[float]) -> float:
     "Product of a list using `reduce` and `mul`."
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    return reduce(mul, 1.0)(ls)
